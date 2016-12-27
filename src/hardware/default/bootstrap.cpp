907678fad88db448be9469fb9076da7720ffc5fa
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2016-12-27 21:44:44 +0200

    Initial commit of base logic

---
+++ src/hardware/default/bootstrap.cpp
@@ -0,0 +1,27 @@
+/**
+ *
+ * CC-Debugger Protocol Library Simulator Utilities
+ * Copyright (c) 2014-2016 Ioannis Charalampidis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <Arduino.h>
+#include <emulated/VirtualSerial.h>
+using namespace std;
+
+VirtualSerial Serial(cin, cout);
+
+/* Here comes the arduino sketch file */
---
+++ src/simulator.cpp
@@ -0,0 +1,33 @@
+/**
+ *
+ * CC-Debugger Protocol Library Simulator Utilities
+ * Copyright (c) 2014-2016 Ioannis Charalampidis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "util/Platform.h"
+
+extern void setup();
+extern void loop();
+
+// Main loop
+int main(int argc, char ** argv) {
+  setup();
+
+  while (1) {
+    loop();
+    sleepcp(25);
+  }
+}
